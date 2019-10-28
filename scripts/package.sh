256f40977004035504b6a404e4b69035fdec1891
Martin Varga <martin.varga@lutraconsulting.co.uk>
2019-10-28 12:25:32 +0100

    updated packaging script to include geodiff binaries for multiple platforms

--- scripts/package.sh
+++ scripts/package.sh
@@ -20,9 +20,25 @@ git checkout $VERSION
 python3 setup.py sdist bdist_wheel
 mkdir -p mergin/deps
 pip wheel -r mergin_client.egg-info/requires.txt -w mergin/deps
+# special care for pygeodiff
 unzip mergin/deps/pygeodiff-*.whl -d mergin/deps
-rm -rf mergin/deps/*.dist-info
-cd ..
+# remove unncesessary files
+rm -rf mergin/deps/*.dist-info mergin/deps/*.data
+# download geodiff binaries for multiple platforms
+GEODIFF_VER="$(geodiff="$(cat mergin_client.egg-info/requires.txt | grep pygeodiff)";echo ${geodiff#pygeodiff==})"
+cd mergin/deps
+mkdir -p tmp
+platforms=(win32 win_amd64 macosx_10_14_x86_64 manylinux2010_x86_64)
+for platform in "${platforms[@]}"
+do
+pip3 download --only-binary=:all: --no-deps --platform ${platform} --python-version 36 --implementation cp --abi cp36m pygeodiff==$GEODIFF_VER
+unzip -o pygeodiff-0.7.4-cp36-cp36m-${platform}.whl -d tmp
+# ignore failures on copy
+cp tmp/pygeodiff/*.{so,dll,dylib} ./pygeodiff 2>/dev/null || :
+rm pygeodiff-0.7.4-cp36-cp36m-${platform}.whl
+done
+rm -r tmp
+cd ../../..
 # create final .zip
 rm -rf Mergin/mergin
 cp -r mergin-py-client/mergin Mergin
