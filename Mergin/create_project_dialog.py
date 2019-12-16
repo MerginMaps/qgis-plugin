baf098370f1cc33e315445646415eee244c98502
PachKosti <razumichin1@gmail.com>
2019-12-16 08:59:18 +0100

    added failure message if username is not stored in settings file yet (closes #62)

--- Mergin/create_project_dialog.py
+++ Mergin/create_project_dialog.py
@@ -42,7 +42,11 @@ class CreateProjectDialog(QDialog):
         project_name = self.ui.project_name.text()
         project_dir = self.ui.project_dir.text() if self.ui.project_dir_btn.isChecked() else None
         QApplication.setOverrideCursor(Qt.WaitCursor)
-        username = settings.value("Mergin/username")
+        username = settings.value("Mergin/username", "")
+
+        if username == "":
+            self._return_failure("Username is not stored. Please save configuration again.")
+            return
 
         if project_dir and '.mergin' in os.listdir(project_dir):
             self._return_failure("Selected directory is already assigned to mergin project.")
