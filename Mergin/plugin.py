517506b09b9d5df1e6c77801a472183e7cc44cad
PachKosti <razumichin1@gmail.com>
2020-01-07 13:03:39 +0100

    returned after message box was displayed

--- Mergin/plugin.py
+++ Mergin/plugin.py
@@ -127,10 +127,11 @@ class MerginProjectItem(QgsDataItem):
         if os.path.exists(self.path):
             try:
                 shutil.rmtree(self.path)
-            except PermissionError as e:
+            except PermissionError:
                 msg = "Failed to delete your project {} because is open.\n" \
                       "Close project and check if it is not open in another application.".format(self.project_name)
                 QMessageBox.critical(None, 'Project delete', msg, QMessageBox.Close)
+                return
 
         settings = QSettings()
         settings.remove('Mergin/localProjects/{}/path'.format(self.project_name))
