1e73fbce5c6c153fb3405e81e7c2ee3e84d42aa5
Radek Pasiok <radek.pasiok@lutraconsulting.co.uk>
2021-03-09 14:51:57 +0100

    Fixed enabling toolbar buttons after config change.

--- Mergin/plugin.py
+++ Mergin/plugin.py
@@ -170,8 +170,8 @@ class MerginPlugin:
 
     def on_config_changed(self):
         """Called when plugin config (connection settings) were changed."""
-        self.enable_toolbar_actions()
         self.create_manager()
+        self.enable_toolbar_actions()
 
     def connect_provider_root_item(self):
         """Set the connection for Mergin config changes."""
@@ -674,8 +674,9 @@ class MerginRootItem(QgsDataCollectionItem):
         if not unsaved_project_check():
             return
         if not self.project_manager:
-            QMessageBox.warning(None, "Mergin Plugin Config Error",
-                                "Mergin plugin configuration is invalid - project manager couldn't be created.")
+            warn = "Mergin plugin has not been configured yet or the configuration is not valid.\n"
+            warn += "Please go to the configuration dialog and set up your Mergin credentials."
+            QMessageBox.warning(None, "Mergin Plugin Config Error", warn)
             return
         user_info = self.mc.user_info()
         self.wizard = NewMerginProjectWizard(
