a8bd164bb3c71499deffadb3bdf55d275a70a283
Alexander Bruy <alexander.bruy@gmail.com>
2022-03-10 07:13:20 +0200

    initial implementation of the reporting tool

---
+++ Mergin/processing/provider.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from qgis.PyQt.QtGui import QIcon
+
+from qgis.core import QgsProcessingProvider
+
+from ..utils import icon_path
+from .algs.create_report import CreateReport
+
+
+class MerginProvider(QgsProcessingProvider):
+
+    def __init__(self):
+        super().__init__()
+        self.algs = []
+
+    def id(self):
+        return 'mergin'
+
+    def name(self):
+        return 'Mergin'
+
+    def icon(self):
+        return QIcon(icon_path('icon.png', False))
+
+    def load(self):
+        self.refreshAlgorithms()
+        return True
+
+    def unload(self):
+        pass
+
+    def supportsNonFileBasedOutput(self):
+        return False
+
+    def getAlgs(self):
+        algs = [CreateReport(),
+               ]
+
+        return algs
+
+    def loadAlgorithms(self):
+        self.algs = self.getAlgs()
+        for a in self.algs:
+            self.addAlgorithm(a)
