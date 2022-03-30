09147f31e94b84f7bc2e66ccb24ff5dafec88710
Alexander Bruy <alexander.bruy@gmail.com>
2022-03-30 12:43:57 +0300

    project status dialog overhaul

--- Mergin/project_status_dialog.py
+++ Mergin/project_status_dialog.py
@@ -1,21 +1,22 @@
 import os
-from PyQt5.QtWidgets import (
+
+from qgis.PyQt import uic
+from qgis.PyQt.QtWidgets import (
     QAbstractItemView,
     QDialog,
     QDialogButtonBox,
-    QLabel,
-    QTabWidget,
-    QTreeView,
-    QVBoxLayout,
-    QHBoxLayout,
-    QWidget,
     QStyle,
-    QSizePolicy
+    QSizePolicy,
+    QPushButton
 )
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtGui import QStandardItemModel, QStandardItem, QIcon
+
+from qgis.gui import QgsGui
 from qgis.core import QgsApplication, QgsProject
 from .utils import is_versioned_file
+
+ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ui', 'ui_status_dialog.ui')
 
 
 class ProjectStatusDialog(QDialog):
@@ -30,82 +31,60 @@ class ProjectStatusDialog(QDialog):
 
     def __init__(
         self, pull_changes, push_changes, push_changes_summary, has_write_permissions, validation_results,
-            mergin_project=None, parent=None
+            mergin_project=None, parent=None, show_sync_button=False
     ):
-        super(ProjectStatusDialog, self).__init__(parent)
+        QDialog.__init__(self, parent)
+        self.ui = uic.loadUi(ui_file, self)
+
+        QgsGui.instance().enableAutoGeometryRestore(self)
+
+        if show_sync_button:
+            self.btn_sync = QPushButton("Sync")
+            self.btn_sync.clicked.connect(self.sync_project)
+            self.ui.buttonBox.addButton(self.btn_sync, QDialogButtonBox.ActionRole)
+
         self.validation_results = validation_results
-        self.setWindowTitle("Project status")
-        self.table = QTreeView()
-        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.mp = mergin_project
+
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(["Status"])
-        self.table.setModel(self.model)
-        self.mp = mergin_project
+        self.treeStatus.setModel(self.model)
 
         self.check_any_changes(pull_changes, push_changes)
         self.add_content(pull_changes, "Server changes", True)
         self.add_content(push_changes, "Local changes", False, push_changes_summary)
-        self.table.expandAll()
+        self.treeStatus.expandAll()
 
-        main_lout = QVBoxLayout(self)
-        self.tabs = QTabWidget()
-        main_lout.addWidget(self.tabs)
-        self.status_tab = QWidget()
-        self.valid_tab = QWidget()
-        self.tabs.addTab(self.status_tab, "Status")
-        self.tabs.addTab(self.valid_tab, "Validation results")
+        if not self.validation_results:
+            self.ui.lblWarnings.hide()
+            self.ui.txtWarnings.hide()
+        else:
+            self.show_validation_results()
 
-        status_lay = QVBoxLayout(self.status_tab)
-        if self.mp.has_unfinished_pull():
-            warn_lay = QHBoxLayout()
-            lbl_warn_icon = QLabel()
-            lbl_warn_icon.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-            icon = self.style().standardIcon(QStyle.SP_MessageBoxWarning)
-            lbl_warn_icon.setPixmap(icon.pixmap(icon.availableSizes()[0]))
-            warn_lay.addWidget(lbl_warn_icon)
-            lbl_unfinished = QLabel()
-            lbl_unfinished.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-            lbl_unfinished.setWordWrap(True)
-            lbl_unfinished.setText(
-                "The previous pull has not finished completely: status "
-                "of some files may be reported incorrectly."
-            )
-            warn_lay.addWidget(lbl_unfinished)
-            status_lay.addLayout(warn_lay)
-
-        status_lay.addWidget(self.table)
         has_files_to_replace = any(
             ["diff" not in file and is_versioned_file(file["path"]) for file in push_changes["updated"]]
         )
-        info_text = self._get_info_text(has_files_to_replace, has_write_permissions)
-        if info_text:
-            text_box = QLabel()
-            text_box.setWordWrap(True)
-            text_box.setText(info_text)
-            status_lay.addWidget(text_box)
+        info_text = self._get_info_text(has_files_to_replace, has_write_permissions, self.mp.has_unfinished_pull())
+        for msg in info_text:
+            self.ui.messageBar.pushWarning("WARNING", msg)
 
-        box = QDialogButtonBox(QDialogButtonBox.Ok, centerButtons=True,)
-        box.accepted.connect(self.accept)
-        box.rejected.connect(self.reject)
-        main_lout.addWidget(box, Qt.AlignCenter)
-
-        self.valid_view = QTreeView()
-        self.valid_view.setStyleSheet("QTreeView::item { padding: 5px }")
-        self.valid_model = QStandardItemModel()
-        self.show_validation_results()
-
-        self.resize(640, 640)
-
-    def _get_info_text(self, has_files_to_replace, has_write_permissions):
-        msg = ""
+    def _get_info_text(self, has_files_to_replace, has_write_permissions, has_unfinished_pull):
+        msg = []
         if not has_write_permissions:
-            msg += f"WARNING: You don't have writing permissions to this project. Changes won't be synced!\n"
+            msg.append(f"You don't have writing permissions to this project. Changes won't be synced!")
 
         if has_files_to_replace:
-            msg += (
-                f"\nWARNING: Unable to compare some of the modified files with their server version - "
+            msg.append(
+                f"Unable to compare some of the modified files with their server version - "
                 f"their history will be lost if uploaded."
             )
+
+        if has_unfinished_pull:
+            msg.append(
+                f"The previous pull has not finished completely: status "
+                f"of some files may be reported incorrectly."
+            )
+
         return msg
 
     def check_any_changes(self, pull_changes, push_changes):
@@ -163,22 +142,19 @@ class ProjectStatusDialog(QDialog):
         return item
 
     def show_validation_results(self):
-        lout = QVBoxLayout(self.valid_tab)
-        lout.addWidget(self.valid_view)
-        self.valid_view.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.valid_model.setHorizontalHeaderLabels(["Validation results"])
-        self.valid_view.setModel(self.valid_model)
-
+        html = []
         map_layers = QgsProject.instance().mapLayers()
         for issues_data in sorted(self.validation_results):
             level, issue = issues_data
             layer_ids = self.validation_results[issues_data]
-            issue_item = QStandardItem(issue)
+            html.append(f"<h3>{issue}</h3>")
+            items = []
             for lid in sorted(layer_ids, key=lambda x: map_layers[x].name()):
                 layer = map_layers[lid]
-                lyr_item = QStandardItem(f"- {layer.name()}")
-                lyr_item.setToolTip(layer.publicSource())
-                issue_item.appendRow(lyr_item)
-            self.valid_model.appendRow(issue_item)
+                items.append(f"<li>{layer.name()}</li>")
+            html.append(f"<ul>{''.join(items)}</ul>")
 
-        self.valid_view.expandAll()
+        self.txtWarnings.setHtml(''.join(html))
+
+    def sync_project(self):
+        pass
