fa6962d5220a9c17251939df08a2e3ec73b99e5b
uclaros <uclaros@gmail.com>
2022-09-29 11:23:16 +0300

    add warning message to clone dialog

--- Mergin/clone_project_dialog.py
+++ Mergin/clone_project_dialog.py
@@ -1,4 +1,5 @@
 import os
+
 from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox, QFileDialog, QApplication, QMessageBox, QComboBox
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt import uic
@@ -12,14 +13,6 @@ class CloneProjectDialog(QDialog):
         self.ui = uic.loadUi(ui_file, self)
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
         self.ui.buttonBox.accepted.connect(self.accept_dialog)
-        if not workspaces:
-            # This means server is old and uses namespaces
-            username = user_info["username"]
-            user_organisations = user_info.get("organisations", [])
-            self.ui.projectNamespace.addItem(username, True)
-            for o in user_organisations:
-                if user_organisations[o] in ["admin", "owner"]:
-                    self.ui.projectNamespace.addItem(o, True)
 
         for ws in workspaces:
             try:
@@ -30,6 +23,16 @@ class CloneProjectDialog(QDialog):
                 # Server is ce, we'll ask for forgiveness instead of permission
                 is_writable = True
                 self.ui.projectNamespace.addItem(ws, is_writable)
+
+        if not workspaces:
+            # This means server is old and uses namespaces
+            username = user_info["username"]
+            user_organisations = user_info.get("organisations", [])
+            self.ui.projectNamespace.addItem(username, True)
+            self.ui.projectNamespaceLabel.setText("Owner")
+            for o in user_organisations:
+                if user_organisations[o] in ["admin", "owner"]:
+                    self.ui.projectNamespace.addItem(o, True)
 
         self.ui.projectNamespace.currentTextChanged.connect(self.workspace_changed)
         self.ui.edit_project_name.textChanged.connect(self.text_changed)
@@ -44,18 +47,19 @@ class CloneProjectDialog(QDialog):
         self.project_namespace = None
 
     def text_changed(self):
-        self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(bool(self.ui.edit_project_name.text()))
+        enabled = bool(self.ui.edit_project_name.text()) and not self.ui.warningMessageLabel.isVisible()
+        self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(enabled)
 
     def workspace_changed(self):
         is_writable = bool(self.ui.projectNamespace.currentData(Qt.UserRole))
-
         if is_writable:
             msg = ""
         else:
             msg = "You do not have write permissions for this workspace"
         self.ui.edit_project_name.setToolTip(msg)
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setToolTip(msg)
-        self.ui.edit_project_name.setEnabled(is_writable)
+        self.ui.warningMessageLabel.setVisible(not is_writable)
+        self.ui.warningMessageLabel.setText(msg)
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(is_writable and bool(self.ui.edit_project_name.text()))
 
     def accept_dialog(self):
