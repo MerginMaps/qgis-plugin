ab23986e1beafe05b0be73129f49fec3c489363f
Valentin Buira <valentin.buira@gmail.com>
2024-07-10 14:50:33 +0200

    Cleanup: Prefer using elif when possible

--- Mergin/projects_manager.py
+++ Mergin/projects_manager.py
@@ -93,7 +93,7 @@ class MerginProjectsManager(object):
             # User friendly error messages
             if e.http_error == 409:
                 msg = f'Project with the name "{project_name}" already exists in the workspace "{namespace}"\n Try renaming the project'
-            if e.http_error == 422 and e.server_code == ErrorCode.ProjectsLimitHit.value:
+            elif e.http_error == 422 and e.server_code == ErrorCode.ProjectsLimitHit.value:
                 msg = (
                     "Maximum number of projects is reached. Please upgrade your subscription to create new projects\n"
                     f"Maximum of projects: {e.server_response["projects_quota"]}"
