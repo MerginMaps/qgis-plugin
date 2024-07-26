c9ad1815130f984aea993c49e28d5ba369ffff2e
Valentin Buira <valentin.buira@gmail.com>
2024-07-26 11:29:43 +0200

    Add utils fonction to convert from byte to mb, used in error message

--- Mergin/utils.py
+++ Mergin/utils.py
@@ -1505,3 +1505,12 @@ def get_layer_by_path(path):
         safe_file_path = layer_path.split("|")
         if safe_file_path[0] == path:
             return layer
+
+def bytes_to_mb(bytes : int):
+    """
+    Convert bytes to megabytes assuming base of bytes in binary
+    
+    Returns:
+    float: Result in megabytes
+    """
+    return bytes / (2**20)
