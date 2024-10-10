1705add49991409b6161e6554d8a963d279a9091
Valentin Buira <valentin.buira@gmail.com>
2024-10-10 14:46:24 +0200

    Format

--- Mergin/projects_manager.py
+++ Mergin/projects_manager.py
@@ -364,8 +364,7 @@ class MerginProjectsManager(object):
             if updated["path"] == qgis_proj_basename:
                 qgis_proj_changed = True
                 break
-        filename_in_files = qgis_proj_filename in find_qgis_files(project_dir)
-        if filename_in_files and qgis_proj_changed:
+        if qgis_proj_filename in find_qgis_files(project_dir) and qgis_proj_changed:
             self.open_project(project_dir)
 
         if dlg.exception:
