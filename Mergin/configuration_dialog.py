07f67906465bf05dcd6a2c345fc0e1b10e978b7b
Jan Caha <jan.caha@lutraconsulting.co.uk>
2025-05-02 09:42:21 +0200

    sso functions - GUI + login

--- Mergin/configuration_dialog.py
+++ Mergin/configuration_dialog.py
@@ -2,9 +2,11 @@
 # Copyright Lutra Consulting Limited
 
 import os
+import json
+import uuid
 from qgis.PyQt.QtWidgets import QDialog, QApplication, QDialogButtonBox, QInputDialog, QMessageBox
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import Qt, QSettings, QUrl
+from qgis.PyQt.QtCore import Qt, QSettings, QUrl, QTimer
 from qgis.PyQt.QtGui import QPixmap
 from qgis.PyQt.QtNetwork import QNetworkRequest
 from qgis.core import QgsApplication, QgsAuthMethodConfig, QgsBlockingNetworkRequest, QgsExpressionContextUtils
@@ -67,9 +69,15 @@ class ConfigurationDialog(QDialog):
         self.ui.save_credentials.stateChanged.connect(self.check_master_password)
         self.ui.username.textChanged.connect(self.check_credentials)
         self.ui.password.textChanged.connect(self.check_credentials)
-        self.check_credentials()
+        self.ui.merginURL.textChanged.connect(self.check_sso_availability)
 
-        self.ui.sso_btn.clicked.connect(self.sso)
+        self.ui.button_sign_sso.clicked.connect(self.show_sign_sso)
+        self.ui.button_sign_password.clicked.connect(self.show_sign_email)
+
+        self.check_credentials()
+        self.allow_sso_login()
+
+        # self.ui.sso_btn.clicked.connect(self.sso)
 
     def accept(self):
         if not self.test_connection():
@@ -139,6 +147,136 @@ class ConfigurationDialog(QDialog):
         QApplication.restoreOverrideCursor()
         self.ui.test_status.setText(msg)
         return ok
+
+    def allow_sso_login(self) -> None:
+        self.ui.button_sign_sso.setVisible(False)
+
+        server_config_data, error_type, error_msg = self.json_response(f"{self.server_url()}/config")
+        if error_type:
+            QMessageBox.critical(self, error_type, error_msg)
+            return
+
+        if "sso_enabled" in server_config_data:
+            sso_enabled = server_config_data["sso_enabled"]
+            if sso_enabled:
+                self.ui.button_sign_sso.setVisible(True)
+
+    def check_sso_availability(self) -> None:
+        self.sso_timer = QTimer(self)
+        self.sso_timer.setSingleShot(True)
+        self.sso_timer.start(1000)
+        self.sso_timer.timeout.connect(self.allow_sso_login)
+
+    def show_sign_sso(self) -> None:
+        self.ui.stacked_widget_login.setCurrentIndex(1)
+
+        self.ui.sso_email.setVisible(self.sso_ask_for_email())
+
+    def show_sign_email(self) -> None:
+        self.ui.stacked_widget_login.setCurrentIndex(0)
+
+    def json_response(self, url: str) -> tuple[dict, str, str]:
+        br = QgsBlockingNetworkRequest()
+        error = br.get(QNetworkRequest(QUrl(url)))
+
+        if error != QgsBlockingNetworkRequest.ErrorCode.NoError:
+            return ({}, "Network Error", "Failed to get SSO config")
+
+        json_raw_data = bytes(br.reply().content())
+
+        try:
+            json_data = json.loads(json_raw_data)
+        except json.JSONDecodeError:
+            return ({}, "Server Response Error", "Server response is not valid JSON")
+
+        return (json_data, "", "")
+
+    def sso_ask_for_email(self) -> bool:
+        json_data, error_type, error_msg = self.json_response(f"{self.server_url()}/v2/sso/config")
+
+        if error_type:
+            QMessageBox.critical(self, error_type, error_msg)
+            return True
+
+        if "tenant_flow_type" not in json_data:
+            QMessageBox.critical(
+                self, "Server Response Error", "Server response did not contain required tenant_flow_type data"
+            )
+            return True
+
+        if json_data["tenant_flow_type"] not in ["multi", "single"]:
+            QMessageBox.critical(self, "Server Response Error", "SSO tenant_flow_type is not valid")
+            return True
+
+        if json_data["tenant_flow_type"] == "multi":
+            return True
+
+        return False
+
+    def login_using_sso(self) -> None:
+        if self.ui.sso_email.isVisible():
+            json_data, error_type, error_msg = self.json_response(
+                f"{self.server_url()}/v2/sso/connections?email={self.ui.sso_email.text()}"
+            )
+        else:
+            json_data, error_type, error_msg = self.json_response(f"{self.server_url()}/v2/sso/config")
+
+        if error_type:
+            QMessageBox.critical(self, error_type, error_msg)
+            return
+
+        if "id" not in json_data:
+            QMessageBox.critical(self, "Server Response Error", "Server response did not contain required id data")
+            return
+
+        oauth2_client_id = json_data["id"]
+
+        # add/update SSO config
+        config_dict = {
+            "accessMethod": 0,
+            "apiKey": "",
+            "clientId": oauth2_client_id,
+            "clientSecret": "",
+            "configType": 1,
+            "customHeader": "",
+            "description": "",
+            "grantFlow": 3,
+            "id": "mmmmsso",
+            "name": "Mergin Maps SSO",
+            "objectName": "",
+            "password": "",
+            "persistToken": False,
+            "queryPairs": {"state": str(uuid.uuid4())},
+            "redirectHost": "localhost",
+            "redirectPort": 8082,
+            "redirectUrl": "qgis",
+            "refreshTokenUrl": "",
+            "requestTimeout": 30,
+            "requestUrl": f"{self.server_url()}/v2/sso/authorize",
+            "scope": "",
+            "tokenUrl": f"{self.server_url()}/v2/sso/token",
+            "username": "",
+            "version": 1,
+        }
+        config_json = json.dumps(config_dict)
+        config = QgsAuthMethodConfig(method="OAuth2")
+        config.setName("Mergin Maps SSO")
+        config.setId("mmmmsso")
+        config.setConfig("oauth2config", config_json)
+        if "mmmmsso" in QgsApplication.authManager().configIds():
+            QgsApplication.authManager().updateAuthenticationConfig(config)
+        else:
+            QgsApplication.authManager().storeAuthenticationConfig(config)
+
+        # trigger OAuth2  (will open browser if QGIS does not have token yet)
+        blocking_request = QgsBlockingNetworkRequest()
+        blocking_request.setAuthCfg("mmmmsso")
+        res = blocking_request.get(QNetworkRequest(QUrl(f"{self.server_url()}/ping")))
+        reply = blocking_request.reply()
+        access_token = bytes(reply.request().rawHeader(b"Authorization"))  # includes "Bearer ...."
+
+        # create mergin client using the token
+        access_token_str = access_token.decode("utf-8")
 
     def sso(self):
 
