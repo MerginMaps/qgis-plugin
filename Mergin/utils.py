9c724022d4a4a669701d56cca63b9c2d41924857
Valentin Buira <30632058+ValentinBuira@users.noreply.github.com>
2025-05-09 09:27:24 +0200

    Deprecate old server version (< 2023) (#697)
    * Add a warning for deprecated old server

--- Mergin/utils.py
+++ Mergin/utils.py
@@ -1250,7 +1250,14 @@ def test_server_connection(url, username, password):
     result = True, "<font color=green> OK </font>"
     proxy_config = get_qgis_proxy_config(url)
     try:
-        MerginClient(url, None, username, password, get_plugin_version(), proxy_config)
+        mc = MerginClient(url, None, username, password, get_plugin_version(), proxy_config)
+
+        if mc.server_type() == ServerType.OLD:
+            QMessageBox.information(
+                None,
+                "Deprecated server version",
+                "This server is running an outdated version that will no longer be supported. Please contact your server administrator to upgrade.",
+            )
     except (LoginError, ClientError) as e:
         QgsApplication.messageLog().logMessage(f"Mergin Maps plugin: {str(e)}")
         result = False, f"<font color=red> Connection failed, {str(e)} </font>"
