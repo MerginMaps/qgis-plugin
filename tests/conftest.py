819c5b15f4e7bb61ff78c9b7c441fc8f46fd3496
Jan Caha <jan.caha@outlook.com>
2026-03-27 15:17:46 +0100

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,12 @@
 # GPLv3 license
 # Copyright Lutra Consulting Limited
 
+import json
 from pathlib import Path
+from typing import Dict
 
 import pytest
-from qgis.core import QgsVectorLayer
+from qgis.core import QgsProject, QgsVectorLayer
 
 
 @pytest.fixture
@@ -37,3 +39,27 @@ def raster_tiles_path(test_data_path: Path) -> Path:
 def vector_tiles_path(test_data_path: Path) -> Path:
     """Fixture for vector tiles MBTiles file path."""
     return test_data_path / "vector-tiles.mbtiles"
+
+
+@pytest.fixture
+def base_schema(test_data_path: Path) -> Dict:
+    """Fixture for base schema used in tests."""
+    with open(test_data_path / "schema_base.json") as f:
+        return json.load(f).get("geodiff_schema")
+
+
+@pytest.fixture
+def tables_schema(test_data_path: Path) -> Dict:
+    """Fixture for tables schema used in tests."""
+    with open(test_data_path / "schema_two_tables.json") as f:
+        return json.load(f).get("geodiff_schema")
+
+
+@pytest.fixture
+def project_dir(mem_layer: QgsVectorLayer, tmp_path: Path) -> Path:
+    """Fixture for a QGIS project directory."""
+    proj = QgsProject.instance()
+    proj.addMapLayer(mem_layer)
+    proj.setFileName(str(tmp_path / "test_project.qgz"))
+    yield tmp_path
+    proj.removeMapLayer(mem_layer.id())
